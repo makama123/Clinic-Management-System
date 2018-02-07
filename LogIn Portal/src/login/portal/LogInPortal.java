60b9bf1accd0c6d7b2c620b6a6b4c0b12ef9d274
makama123 <33891480+makama123@users.noreply.github.com>
2018-02-07 16:31:06 +0300

    Add files via upload

---
+++ LogIn Portal/src/login/portal/LogInPortal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login.portal;
+
+/**
+ *
+ * @author Denver Makama
+ */
+public class LogInPortal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
